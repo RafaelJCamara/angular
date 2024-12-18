5c7f45f838384dedca09332af0aca4d052fb19c6
RafaelJCamara <rafaelcamarac@gmail.com>
2024-12-18 21:31:31 -0100

    test: add test for getter invocation in text interpolation

--- packages/compiler-cli/src/ngtsc/typecheck/extended/test/checks/uninvoked_function_in_text_interpolation/uninvoked_function_in_text_interpolation_spec.ts
+++ packages/compiler-cli/src/ngtsc/typecheck/extended/test/checks/uninvoked_function_in_text_interpolation/uninvoked_function_in_text_interpolation_spec.ts
@@ -88,5 +88,34 @@ runInEachFileSystem(() => {
       expect(diags[0].code).toBe(ngErrorCode(ErrorCode.UNINVOKED_FUNCTION_IN_TEXT_INTERPOLATION));
       expect(getSourceCodeForDiagnostic(diags[0])).toBe('firstName');
     });
+
+    it('should not produce a warning when a getter is invoked in text interpolation', () => {
+      const fileName = absoluteFrom('/main.ts');
+      const {program, templateTypeChecker} = setup([
+        {
+          fileName,
+          templates: {
+            'TestCmp': `<p> {{ firstName }} </p>`,
+          },
+          source: `
+          export class TestCmp {
+            get firstName() {
+              return 'Morgan';
+            }
+          }`,
+        },
+      ]);
+      const sf = getSourceFileOrError(program, fileName);
+      const component = getClass(sf, 'TestCmp');
+      const extendedTemplateChecker = new ExtendedTemplateCheckerImpl(
+        templateTypeChecker,
+        program.getTypeChecker(),
+        [uninvokedFunctionInTextInterpolationFactory],
+        {},
+        /* options */
+      );
+      const diags = extendedTemplateChecker.getDiagnosticsForComponent(component);
+      expect(diags.length).toBe(0);
+    });
   });
 });
